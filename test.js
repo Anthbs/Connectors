1dddda48e9cdda4b7e030b5c34f3d8f3e8ba5cef
Anthony Barr-Smith <anthony.barrsmith@agfirstbop.co.nz>
2015-01-13 12:03:02 +1300

    Added test file

---
+++ test.js
@@ -0,0 +1,18 @@
+/*jslint node: true */
+"use strict";
+
+var Connectors = require('./Libs/Connectors');
+
+var ms_connector = new Connectors.In.MySql("localhost", 3306, "root", "Anthonybs123");
+ms_connector.Connect().then(function (connection) {
+    ms_connector.Databases().then(function (databases) {
+        console.dir({
+            databases: databases
+        });
+        databases.forEach(function (database) {
+            ms_connector.Models(database).then(function (models) {
+                console.log();
+            });
+        });
+    });
+});
